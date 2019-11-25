1fe2015d5878d809991623e8349d76eb06bec3dd
dos Santos <rdds@gft.com>
2019-11-25 19:55:06 -0200

    movendo as exceptions para o projeto core

--- micro-core/src/main/java/br/com/zenganet/core/exceptions/CoreExceptionHandler.java
+++ micro-core/src/main/java/br/com/zenganet/core/exceptions/CoreExceptionHandler.java
@@ -1,4 +1,4 @@
-package br.com.zenganet.cadastro.exceptions;
+package br.com.zenganet.core.exceptions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,7 +24,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class CadastroExceptionHandler extends ResponseEntityExceptionHandler{
+public class CoreExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@Autowired
 	private MessageSource messageSource;
